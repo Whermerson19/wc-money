630c5e8583fc00b22a4add50c1a7abc9caf6a7c7
Whermerson19 <whermersonc@gmail.com>
2021-04-04 08:35:25 -0300

    component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <div>
-      Hello world
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   )
 }
