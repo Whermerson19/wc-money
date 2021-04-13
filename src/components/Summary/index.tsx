c3c30c56b53009cfab285772d9d8ddd455dae1e5
Whermerson19 <whermersonc@gmail.com>
2021-04-13 11:55:24 -0300

    finish sumary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,44 +1,71 @@
-import { useContext } from 'react'
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext'
+import { useContext, useMemo } from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContext";
 
-import { Container, Card } from './styles'
+import { Container, Card } from "./styles";
 
 export default function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const incomes = useMemo(() => {
+    return transactions.reduce((accumulator, transaction) => {
+      if (transaction.type === 'deposit') {
+        accumulator += transaction.amount
+      }
+
+      return accumulator
+    }, 0)
+  }, [transactions]);
+
+  const outcomes = useMemo(() => {
+    return transactions.reduce((accumulator, transaction) => {
+      if (transaction.type === 'withdraw') {
+        accumulator += transaction.amount
+      }
+
+      return accumulator
+    }, 0)
+  }, [transactions]);
 
   return (
     <Container>
       <Card>
         <header>
           <p>Entradas</p>
-          <img src={incomeImg} alt="income"/>
+          <img src={incomeImg} alt="income" />
         </header>
 
-        <strong>R$ 1000,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(incomes)}</strong>
       </Card>
 
       <Card>
         <header>
           <p>Saídas</p>
-          <img src={outcomeImg} alt="outcome"/>
+          <img src={outcomeImg} alt="outcome" />
         </header>
 
-        <strong>- R$ 500,00</strong>
+        <strong>-{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(outcomes)}</strong>
       </Card>
 
       <Card isTotal>
         <header>
           <p>Total</p>
-          <img src={totalImg} alt="total"/>
+          <img src={totalImg} alt="total" />
         </header>
 
-        <strong>R$ 500,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format((incomes - outcomes))}</strong>
       </Card>
     </Container>
-  )
+  );
 }
